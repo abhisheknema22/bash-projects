590e3cd92d593d26ce7b4d3765ca95cf17f660c9
Abhishek Nema <nemaabhishek3@gmail.com>
2026-05-10 08:27:05 +0000

    updated project-1 code

--- project-1.sh
+++ project-1.sh
@@ -6,26 +6,26 @@
 #
 # Date: 10-05-2026
 #
-# This script installs git software in O.S. by checking it's flavour
+# This script installs docker in O.S. by checking it's flavour
 #
 # Version: v1
 #
 #################
 
-echo "script to install git"
-echo "Installing git"
+echo "script to install Docker"
+echo "Installing docker.io"
 
 if [[ "$(cat /etc/os-release | grep PRETTY_NAME=)"  == "Ubuntu 26.04 LTS" ]];
 then
 	echo "This is Ubuntu"
-	sudo apt update && apt install git -y
+	sudo apt update && apt install docker.io -y
 
 elif [[ "$(cat /etc/os-release | grep PRETTY_NAME=)"  == "Amazon Linux 2023.11.20260509" ]];
 then
 	echo "This is Amazon Linux"
-	sudo dnf update && dnf install git -y
+	sudo dnf update && dnf install docker.io -y
 
 else
 	echo "Something went wrong!"
-	echo "Not installing git"
+	echo "Not installing Docker"
 fi
