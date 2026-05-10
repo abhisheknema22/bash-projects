9834e6660d3c3a0b72391293bef4c37e6dd9e1fd
Abhishek Nema <nemaabhishek3@gmail.com>
2026-05-10 08:49:06 +0000

    updated project-1 code

--- project-1.sh
+++ project-1.sh
@@ -18,12 +18,12 @@ echo "Installing docker.io"
 if [[ "$(cat /etc/os-release | grep ^NAME=)"  == 'NAME="Ubuntu"' ]];
 then
 	echo "This is Ubuntu"
-	sudo apt update && apt install docker.io -y
+	sudo apt update && sudo apt install docker.io -y
 
 elif [[ "$(cat /etc/os-release | grep ^NAME=)"  == 'NAME="Amazon Linux"' ]];
 then
 	echo "This is Amazon Linux"
-	sudo dnf update && dnf install docker -y
+	sudo dnf update && sudo dnf install docker -y
 
 else
 	echo "Something went wrong!"
